9df80ee781b56795aac2675a206014b621f8ddbc
fransiskarpl <fransiskapklrpl@gmail.com>
2026-08-06 11:35:48 +0700

    Initial commit

---
+++ GlowList-Backend/server.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql2');
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang di GlowList API 💄');
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+/// khusus produk
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.post('/produk', (req, res) => {
+  const { judul, deskripsi, harga, id_kategori } = req.body;
+
+  if (!judul || !harga) {
+      return res.status(400).json({
+        message: 'Judul dan harga wajib diisi'
+      });
+    }
+
+  if (!deskripsi) {
+      return res.status(400).json({
+        message: 'Deskripsi wajib diisi'
+      });
+    }
+
+  const sql = `
+    INSERT INTO produk
+    (judul, deskripsi, harga, id_kategori, tgl_input)
+    VALUES (?, ?, ?, ?, NOW())
+  `;
+
+  db.query(
+    sql,
+    [judul, deskripsi, harga, id_kategori],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.sqlMessage
+        });
+      }
+
+      res.json({
+        message: 'Produk berhasil ditambahkan!',
+        id_produk: result.insertId
+      });
+    }
+  );
+});
+
+app.put("/produk/:id_produk", (req, res) => {
+    const { id_produk } = req.params;
+    const { judul, deskripsi, harga, id_kategori } = req.body;
+
+    if (!judul || !harga) {
+        return res.status(400).json({
+            message: "Judul dan harga wajib diisi",
+        });
+    }
+
+    const sql = `
+        UPDATE produk
+        SET judul = ?, deskripsi = ?, harga = ?, id_kategori = ?
+        WHERE id_produk = ?
+    `;
+
+    db.query(
+        sql,
+        [judul, deskripsi, harga, id_kategori, id_produk],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.sqlMessage,
+                });
+            }
+
+            res.json({
+                message: "Produk berhasil diupdate!",
+            });
+        }
+    );
+});
+
+app.delete("/produk/:id_produk", (req, res) => {
+    const { id_produk } = req.params;
+
+    const sql = "DELETE FROM produk WHERE id_produk = ?";
+
+    db.query(sql, [id_produk], (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.sqlMessage,
+            });
+        }
+
+        res.json({
+            message: "Produk berhasil dihapus!",
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server GlowList jalan di http://localhost:${PORT}');
+});
+
---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,9 @@
+// Tentang.jsx
+export default function Tentang() {
+  return (
+    <div>
+      <h2>Profil Singkat Tentang Saya</h2>
+      <p>Haii, nama saya Fransiska Amanah dari kelas XII RPL 1. saya sangat suka menggunakan Skincare karena dapat merawat kulit agar lebih sehat.</p>
+    </div>
+  );
+}
